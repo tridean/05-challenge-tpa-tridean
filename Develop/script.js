41ca1c80ef9fbf0f6466bf9121593342a3726c55
tridean <tridean97@gmail.com>
2023-08-06 19:56:36 -0500

    function to display current date and time

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const localeSettings = {};
+dayjs.locale(localeSettings);
 $(function () {
+  const currentHour = dayjs().format('H');
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,5 +22,17 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
+
   // TODO: Add code to display the current date in the header of the page.
+  function updateTime() {
+    const dateEle = $("#currentDay");
+    const timeEle = $("#currentTime");
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    const currentTime = dayjs().format('hh:mm:ss A');
+    dateEle.text(currentDate);
+    timeEle.text(currentTime);
+  }
+  setInterval(updateTime, 1000);
 });
